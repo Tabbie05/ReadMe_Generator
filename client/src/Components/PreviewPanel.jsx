d24bd913531370192ad8864e7bc2de33678b9168
Tabbie05 <shaikhtayba3@gmail.com>
2025-07-21 13:18:31 +0530

    updated code

--- client/src/Components/PreviewPanel.jsx
+++ client/src/Components/PreviewPanel.jsx
@@ -4,15 +4,15 @@ import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
 import rehypeHighlight from "rehype-highlight";
 import CopyButton from "./CopyButton";
-
+import '../index.css'
 function PreviewPanel({ value }) {
   return (
     <div>
       <section className="relative overflow-y-scroll bg-white p-4 border h-[448px] w-[440px] mr-2 rounded-lg">
         <div className="pt-1 markdown">
           <ReactMarkdown
-            remarkPlugins={[remarkGfm]}
-            rehypePlugins={[rehypeRaw, rehypeHighlight]}
+            // remarkPlugins={[remarkGfm]}
+             rehypePlugins={[rehypeRaw, rehypeHighlight]}
           >
             {value}
           </ReactMarkdown>
